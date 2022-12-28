ded455cc00686b34a52462a53acb5a2f7a3ab886
Ali <alitaghizada2@gmail.com>
2022-12-28 09:47:45 +0400

    added while, if

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // Первая задача
 let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?")
@@ -13,12 +13,34 @@ const personalMovieDB = {
 }
 
 // Третья задача
-let lastWatchedMovies = prompt("Один из последних просмотренных фильмов?");
-let ratingLastWatchedMovies = +prompt("На сколько оцените его?");
-personalMovieDB.movies[lastWatchedMovies] = ratingLastWatchedMovies;
 
-let lastWatchedMovies2 = prompt("Один из последних просмотренных фильмов?");
-let ratingLastWatchedMovies2 = +prompt("На сколько оцените его?");
-personalMovieDB.movies[lastWatchedMovies2] = ratingLastWatchedMovies2;
+let lastWatchedMovies;
+let ratingLastWatchedMovies;
+while (true) {
+    lastWatchedMovies = prompt("Один из последних просмотренных фильмов? Если хотите прервать - введите 'q'");
+    if (lastWatchedMovies == 'q') {break;}
+    if (lastWatchedMovies.length <= 0 || lastWatchedMovies == " "){
+        alert("Нельзя вводить пустое значение.")
+        continue;
+    }
+
+    ratingLastWatchedMovies = +prompt("На сколько оцените его?");
+    if (ratingLastWatchedMovies == 0) {break;}
+    if (ratingLastWatchedMovies.length <= 0 || ratingLastWatchedMovies == " "){
+        alert("Нельзя вводить пустое значение.")
+        continue;
+    }
+    personalMovieDB.movies[lastWatchedMovies] = ratingLastWatchedMovies;
+}
+
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов.")
+} else if (personalMovieDB.count >= 10 & personalMovieDB.count <= 30) {
+    alert("Вы классический зритель!")
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман!")
+} else {
+    alert("Произошла ошибка.")
+}
 
 console.log(personalMovieDB.movies)
